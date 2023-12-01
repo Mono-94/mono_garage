ad3632a730580dc67ecad5d41c95a9a84bd36b08
MONO <68543161+Mono-94@users.noreply.github.com>
2023-12-01 19:14:03 +0100

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -1,5 +1,24 @@
 
 
+
+---- ESX DEFAULT
+-- 
+-- Table structure for table `owned_vehicles`
+--
+
+CREATE TABLE `owned_vehicles` (
+  `owner` varchar(60) DEFAULT NULL,
+  `plate` varchar(12) NOT NULL,
+  `vehicle` longtext DEFAULT NULL,
+  `type` varchar(20) NOT NULL DEFAULT 'car',
+  `job` varchar(20) DEFAULT NULL,
+  `stored` tinyint(4) NOT NULL DEFAULT 0,
+  `parking` VARCHAR(60) DEFAULT NULL,
+  `pound` VARCHAR(60) DEFAULT NULL
+) ENGINE=InnoDB;
+
+
+-- mono garage
 ALTER TABLE `owned_vehicles`
     ADD COLUMN `infoimpound` longtext DEFAULT NULL,
     ADD COLUMN `lastparking` longtext DEFAULT NULL,
